19202fb77bd40ffc3caf4627a73420c3efb0e79b
nisum <nisum@192.168.1.133>
2016-12-26 14:24:00 +0530

    udpated

--- bootTest/src/main/java/hello/ControllerTest.java
+++ bootTest/src/main/java/hello/ControllerTest.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController {
+public class ControllerTest {
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
